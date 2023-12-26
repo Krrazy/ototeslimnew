bdd624862d4dff49d5c992f57d185970bdd0a950
Krrazy <serdalnazilli9@gmail.com>
2023-12-26 19:32:36 +0300

    ototeslimnew
    ototeslimnew

---
+++ src/data/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    
+
+    ACTIVITY_NAME: "🫀 .gg/nitrobase ",
+    BOT_SAHIPLERI: ["900720172053180416"],
+    SELFBOT_TOKENS: ["MTE3Njg4MjQxODMwMTUzODMwNg.G6TZ4X.EFTHtQGI6NzWzcEl1UnRWRaRu4dlNwlmh5jG1o"],
+    
+    DISCORD_BOT_TOKEN: "MTE4ODUyMTkwMjg4MTM5MDY5Mg.G7yKYr.E6shKxphiOMaPqjMHJ1p6z2M9lfOg4D6rFRaVg",
+    DISCORD_CLIENT_ID: "1188521902881390692",
+    MONGO_URI: "mongodb+srv://GhosTedarikTwo:GhosTedarikTwo@ghostedariktwo.mipelxj.mongodb.net/?retryWrites=true&w=majority", // değiştirmenize gerek yok
+    
+    SUNUCU_ID: "1188554482636837085",
+    TESLIMAT_KANALI_ID: "1188559496331669554",
+    LOG_KANALI_ID: "1188556226854932510",
+    LOG_CHANNEL_WEBHOOK_LINK: "https://discord.com/api/webhooks/1188598403639296101/u-uN7gndmaJDJoCJ2oMKfFVIr2i9rb3SMkSraviW0gUG9EmF5Xeltngue_YT3LCml06T",
+    SISTEM_LOG_KANALI_ID: "1188556226854932510",
+    KOD_YAZILACAK_TESLIM_KANALI: "1188557536253710487",
+
+    TESLIMAT_SEKLI: "owo give {user} {cash}",
+    WEBHOOK_KOD_ONAYLANDI_BILDIRIMI: "# {emoji} Kod onaylandı {member}, lütfen bekleyin.",
+    WEBHOOK_TESLIM_BILDIRIMI: ":bell: __İşlem tamamlandı,__ **owo cash** __yazarak gelip gelmediğini kontrol edin.__\n\n<a:shop_kalp:1188603078992199700> `Siparişinizi onaylayıp değerlendirme yapmayı unutmayın!`",
+    WEBHOOK_HATA_BILDIRIMI: "Paranız şu anda gönderilemiyor, lütfen size yardımcı olması için bir yetkiliyi etiketleyin.",
+
+    OWO_BOT_ID: "408785106942164992",
+
+};
